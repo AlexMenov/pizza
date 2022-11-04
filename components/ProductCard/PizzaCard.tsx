b276e35a4c4a7c021b15526459f66a6f6d0567d7
Alexei Mechetin <menovlex@mail.ru>
2022-11-04 22:53:01 +0700

    changed type

--- components/ProductCard/PizzaCard.tsx
+++ components/ProductCard/PizzaCard.tsx
@@ -2,9 +2,8 @@ import Image from 'next/image';
 import { PizzaCardStyled } from './PizzaCard.style';
 import { Htag } from '../Htag/Htag';
 import { Paragraph } from '../Paragraph/Paragraph';
-import { indexProps } from '../../pages/index.props';
 import Link from 'next/link';
-export const PizzaCard = ({  _id, title, desсription, img, prices }: indexProps): JSX.Element => {
+export const PizzaCard = ({  _id, title, desсription, img, prices }: any): JSX.Element => {
     return <PizzaCardStyled>
         <Link href={`/products/pizza/${_id}`}>
             <a>
--- components/ProductList/PizzaList.tsx
+++ components/ProductList/PizzaList.tsx
@@ -1,10 +1,9 @@
 import { PizzaListStyled } from "./Pizza.style";
 import { PizzaCard } from "../ProductCard/PizzaCard";
-import { indexProps } from "../../pages/index.props";
 
 export const PizzaList = ({ pizza }: any) => {
     return <PizzaListStyled>
-        {pizza.result?.map(({ _id, title, desсription, img, prices, extraOptions }: indexProps) => {
+        {pizza.result?.map(({ _id, title, desсription, img, prices, extraOptions }: any) => {
             return <PizzaCard title={title} img={img} key={_id} desсription={desсription} prices={prices} extraOptions={extraOptions} _id={_id}/>;
         })}
     </PizzaListStyled>;
